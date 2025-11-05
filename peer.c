a3f98a9acf161813bb6e38f39cc86760dc2723d5
Sugen Jey <sugenjey@gmail.com>
2025-11-05 12:42:50 -0500

    add menu for peer

--- peer.c
+++ peer.c
@@ -27,7 +27,7 @@ void reaper(int);
 
 int main(int argc, char **argv)
 {
-       char    *host = "localhost";
+    char    *host = "localhost";
     char ans[BUFLEN];
     int port = 3000;
     struct hostent  *phe;   /* pointer to host information entry    */
@@ -55,19 +55,63 @@ int main(int argc, char **argv)
         sin.sin_port = htons(port);
                                                                                        
     /* Map host name to IP address, allowing for dotted decimal */
-        if ( phe = gethostbyname(host) ){
-                memcpy(&sin.sin_addr, phe->h_addr, phe->h_length);
-        }
-        else if ( (sin.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE )
-        fprintf(stderr, "Can't get host entry \n");
-                                                                               
+    if ( phe = gethostbyname(host) ){
+            memcpy(&sin.sin_addr, phe->h_addr, phe->h_length);
+    }
+    else if ( (sin.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE )
+    fprintf(stderr, "Can't get host entry \n");
+                                                                            
     /* Allocate a socket */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
-        if (s < 0)
-        fprintf(stderr, "Can't create socket \n");
-   
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    fprintf(stderr, "Can't create socket \n");
+
                                                                                
     /* Connect the socket */
-        if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-        fprintf(stderr, "Can't connect to %s %s \n", host, "Time");
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    fprintf(stderr, "Can't connect to %s %s \n", host, "Time");
+
+    fd_set rfds, afds;
+    FD_ZERO(&afds);
+    FD_SET(s, &afds); /* Listening on a UDP socket */
+    FD_SET(0, &afds); /* Listening on stdin */
+    while(true)
+    {
+        printf("What would you like to do next?(Enter Number)\n:
+            1. Register Content \n
+            2. Request Content Download\n 
+            3. De-register Content\n
+            4. List all Content Available\n
+            5. Exit\n");
+        memcpy(&rfds, &afds, sizeof(rfds));
+        select(FD_SETSIZE, &rfds, NULL, NULL, NULL);
+        if (FD_ISSET(s, &rfds)) {
+            /* Socket is readable */
+            n = recv(s, ans, BUFLEN, 0);
+            ans[n] = 0;
+            printf("From server: %s", ans);
+        }
+        if (FD_ISSET(0, &rfds)) {
+            n = read(0, buf, BUFSIZE);
+            buf[n] = 0;
+            if (strncmp(buf, "1", 1) == 0) {
+                //Register code
+            }
+            else if (strncmp(buf, "2", 1) == 0) {
+                //Download code
+            }
+            else if (strncmp(buf, "3", 1) == 0) {
+                //De - Register code
+            }
+            else if (strncmp(buf, "4", 1) == 0) {
+                //List ContentS code
+            }
+            else if (strncmp(buf, "5", 1) == 0) {
+                //exit code
+            }
+            else{
+                printf("Invalid Option. Please try again.\n");
+            }
+        }
+    }
 }
